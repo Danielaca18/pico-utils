f3ec0a8cef7fa4cad3a3367d027899aef479426e
Daniel Castro <dcastro@ualberta.ca>
2024-06-13 21:52:27 -0600

    added testing logic to main

--- src/main.c
+++ src/main.c
@@ -1,1 +1,19 @@
-int main() {};
+#include <stdio.h>
+#include "pico/stdio.h"
+#include "bootsel.h"
+
+int main() {
+    stdio_init_all();
+
+    static bool button_state;
+    button_state = false;
+
+    while (true) {
+        if (button_state != get_bootsel_button()) {
+            button_state = !button_state;
+            printf("Button is %s\n", button_state?"on":"off");
+        }
+    }
+
+    return 1;
+}
