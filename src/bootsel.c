f9f6e1a9dec6084835d9ecc90902a2a64bc224eb
Daniel Castro <dcastro@ualberta.ca>
2024-08-04 17:35:47 -0600

    decreased delay after setting cs impedence

--- src/bootsel.c
+++ src/bootsel.c
@@ -4,7 +4,7 @@ bool __no_inline_not_in_flash_func(get_bootsel_button)() {
     const uint CS_PIN_INDEX = 1;
     uint32_t flags = save_and_disable_interrupts();
     setCSImp(CS_PIN_INDEX, 0);
-    inlineSleep(1000);
+    inlineSleep(10);
 
     // The HI GPIO registers in SIO can observe and control the 6 QSPI pins.
     bool button_state = !(sio_hw->gpio_hi_in & (1u << CS_PIN_INDEX));
